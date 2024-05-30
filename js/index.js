24a1fdfe6cd033c7f79476d95819727ebee5bcc4
Roman <kurpelroma@gmail.com>
2024-05-30 17:03:28 +0300

    created and adapted advanteges, calculation sections

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const headerContent = document.querySelector('.header__content')
+const counters = document.querySelectorAll('.adbantages__list-item strong');
+const section = document.querySelector('.advantages');
 
 
-document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
     this.querySelector('.custom-select').classList.toggle('open');
 });
 
 
 for (const option of document.querySelectorAll(".custom-option")) {
-    option.addEventListener('click', function() {
+    option.addEventListener('click', function () {
         if (!this.classList.contains('selected') && !this.classList.contains('disabled')) {
             const selectedOption = this.parentNode.querySelector('.custom-option.selected');
             selectedOption.classList.remove('selected', 'disabled');
@@ -17,7 +19,7 @@ for (const option of document.querySelectorAll(".custom-option")) {
     });
 }
 
-window.addEventListener('click', function(e) {
+window.addEventListener('click', function (e) {
     const select = document.querySelector('.custom-select');
     if (!select.contains(e.target)) {
         select.classList.remove('open');
@@ -33,6 +35,73 @@ function toggleMenu() {
     headerContent.classList.toggle('active');
     btn.classList.toggle('active');
     btn.classList.contains('active') ? document.body.classList.add('body-block') : document.body.classList.remove('body-block')
-  }
-  
-  
+}
+
+
+
+/*Лічильник*/
+
+const formatNumber = (num) => {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            counters.forEach(counter => {
+                const target = +counter.getAttribute('data-target');
+                let count = 0;
+                let increment;
+                let delay;
+
+                if (target > 100000) {
+                    increment = 2500;
+                    delay = 20;
+                } else if (target > 50000) {
+                    increment = 500;
+                    delay = 10;
+                } else if (target > 10000) {
+                    increment = 500;
+                    delay = 15;
+                } else if (target > 5000) {
+                    increment = 50;
+                    delay = 20;
+                } else if (target > 1000) {
+                    increment = 10;
+                    delay = 30;
+                } else {
+                    increment = 1;
+                    delay = 50;
+                }
+
+                const updateCounter = () => {
+                    count += increment;
+                    if (count < target) {
+                        counter.textContent = formatNumber(count);
+                        setTimeout(updateCounter, delay);
+                    } else {
+                        counter.textContent = formatNumber(target);
+                    }
+                };
+
+                updateCounter();
+            });
+
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(section);
+
+
+
+
