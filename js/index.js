832ecc7a99f79ec38ffd76bb9f2ccb72e59ee882
Roman <kurpelroma@gmail.com>
2024-06-26 22:29:31 +0300

    create catalog page (desktop)

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ window.addEventListener('click', function (e) {
         if (isActive) {
             text.style.maxHeight = null;
             text.classList.remove('active')
-            return 
+            return
         }
         if (text.style.maxHeight) {
             text.style.maxHeight = null;
@@ -44,6 +44,43 @@ window.addEventListener('click', function (e) {
             text.style.maxHeight = text.scrollHeight + 'px';
             text.style.marginTop = '1.5rem';
 
+        }
+    }
+
+    const filterBtn = e.target.closest('.filter__drop')
+
+    if (filterBtn) {
+        filterBtn.classList.toggle('active')
+        const content = filterBtn.nextElementSibling
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    }
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (e.target.closest('.header__menu-items li') && isMobile) {
+        const li = e.target.closest('.header__menu-items li')
+        const menu = li.querySelector('.sub__menu')
+
+        if (menu.style.maxHeight) {
+            menu.style.maxHeight = null;
+            menu.style.marginTop = null;
+        } else {
+            menu.style.maxHeight = menu.scrollHeight + 'px';
+            menu.style.marginTop = '1.5rem';
+        }
+
+    }
+
+    const product__list = document.querySelector('.catalog__content')
+
+    if (e.target.closest('.settings__button-show button')) {
+        sidebar.classList.toggle('hide')
+        if (product__list) {
+            product__list.classList.toggle('catalog__content--full')
         }
     }
 
@@ -58,7 +95,7 @@ function toggleMenu() {
 
 
 
-/*Лічильник*/
+/*COUNTER*/
 
 const formatNumber = (num) => {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
@@ -118,9 +155,128 @@ const options = {
 };
 
 const observer = new IntersectionObserver(callback, options);
+if (section) {
+    observer.observe(section);
+}
 
-observer.observe(section);
+/*SUB MENU */
+
+document.querySelectorAll('.header__menu-items li').forEach(item => {
+    let closeMenuTimeout;
+
+    item.addEventListener('mouseenter', () => {
+        const submenu = item.querySelector('.sub__menu');
+        if (submenu) {
+            if (closeMenuTimeout) {
+                clearTimeout(closeMenuTimeout);
+            }
+            submenu.classList.add('active');
+        }
+    });
+
+    item.addEventListener('mouseleave', () => {
+        const submenu = item.querySelector('.sub__menu');
+        if (submenu) {
+            // Встановлюємо таймаут на закриття підменю
+            closeMenuTimeout = setTimeout(() => {
+                submenu.classList.remove('active');
+            }, 500);
+        }
+    });
+
+});
+
+
+/*XFIXED SIDEBAR */
+
+const sidebar = document.querySelector('.sidebar__catalog');
+const catalogContent = document.querySelector('.catalog__content');
+
+const sidebarHeight = sidebar.offsetHeight;
+const catalogContentTopHeight = catalogContent.getBoundingClientRect().top
+const catalogContentBottomHeight = catalogContent.getBoundingClientRect().bottom
+
+function updateSidebarPosition() {
+    const currentScroll = window.pageYOffset;
+    const sidebar = document.querySelector('.sidebar__catalog');
+    const catalogContent = document.querySelector('.catalog__content');
+
+    const catalogContentTopHeight = catalogContent.getBoundingClientRect().top + window.pageYOffset;
+    const catalogContentBottomHeight = catalogContentTopHeight + catalogContent.offsetHeight;
+    const sidebarHeight = sidebar.offsetHeight;
+    const catalogContentBottomHeightMargin = catalogContentBottomHeight - sidebarHeight - 50
+
+
+    if (currentScroll >= catalogContentTopHeight - 50 && currentScroll < catalogContentBottomHeightMargin) {
+        sidebar.classList.add('fixed');
+        sidebar.classList.remove('bottom');
+    } else if (currentScroll >= catalogContentBottomHeightMargin) {
+        sidebar.classList.remove('fixed');
+        sidebar.classList.add('bottom');
+    } else {
+        sidebar.classList.remove('fixed');
+        sidebar.classList.remove('bottom');
+    }
+}
+
+updateSidebarPosition()
+
+// Слухач подій для прокрутки сторінки
+window.addEventListener('scroll', updateSidebarPosition);
 
 
 
 
+
+/*FILTER PRICE */
+
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+    priceInput = document.querySelectorAll(".price-input input"),
+    range = document.querySelector(".slider .progress");
+let priceGap = 1000;
+priceInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minPrice = parseInt(priceInput[0].value),
+            maxPrice = parseInt(priceInput[1].value);
+
+        if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max) {
+            if (e.target.className === "input-min") {
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+            } else {
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+        if ((maxVal - minVal) < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
+
+var sortWrapper = document.querySelector('.settings__button-sort');
+
+sortWrapper.addEventListener('mouseover', function () {
+    sortWrapper.classList.add('active');
+});
+
+sortWrapper.addEventListener('mouseout', function () {
+        sortWrapper.classList.remove('active');
+});
